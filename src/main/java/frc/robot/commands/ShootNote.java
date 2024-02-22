daf362f4503a05dfff7dfe0a6ba5b2590f56d4d0
Dave Billin <db77junk@gmail.com>
2024-02-21 21:31:49 -0800

    Add skeleton code for IntakeNote and ShootNote commands

---
+++ src/main/java/frc/robot/commands/ShootNote.java
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.intake.IntakeSubsystem;
+import frc.robot.subsystems.pivot.PivotSubsystem;
+
+/** Command composition used to shoot a note out of the intake assembly */
+public class ShootNote extends SequentialCommandGroup {
+  
+  /** Creates an instance of the command
+   * @param intake  IntakeSubsystem to operate on
+   * @param pivot  PivotSubsystem to operate on
+   */
+  public ShootNote(IntakeSubsystem intake, PivotSubsystem pivot) {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands();
+  }
+}
